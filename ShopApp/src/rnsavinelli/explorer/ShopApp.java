0e689a430e5aa0c4fe50ada3c9073b704adf1220
R. Nicolás Savinelli <rsavinelli@frba.utn.edu.ar>
2021-08-31 15:44:17 -0300

    refactor

--- ShopApp/src/rnsavinelli/explorer/ShopApp.java
+++ ShopApp/src/rnsavinelli/explorer/ShopApp.java
@@ -11,12 +11,22 @@ package rnsavinelli.explorer;
  */
 public class ShopApp {
     static String welcomeMessage = "Welcome to rnsavinelli.explorer.ShopApp!";    
-    static double tax = 0.2;
 
     private static void greet() {
         System.out.println(welcomeMessage);
     }    
 
+    private static Clothing[] getClothingItemList() {        
+        Clothing item0 = new Clothing("Blue Jacket", 20.9, "M");
+        Clothing item1 = new Clothing("Orange T-Shirt", 10.5, "S");
+        Clothing item2 = new Clothing("Green Scarf", 5, "S");
+        Clothing item3 = new Clothing("Blue T-Shirt", 10.5, "S");
+        
+        Clothing items[] = {item0, item1, item2, item3};
+        
+        return items;
+    }
+    
     /**
      * @param args the command line arguments
      */
@@ -29,23 +39,12 @@ public class ShopApp {
            
         greet();
                         
-        Clothing item0 = new Clothing("Blue Jacket", 20.9, "M");
-        Clothing item1 = new Clothing("Orange T-Shirt", 10.5, "S");
-        Clothing item2 = new Clothing("Green Scarf", 5, "S");
-        Clothing item3 = new Clothing("Blue T-Shirt", 10.5, "S");
+        Clothing[] items = getClothingItemList();
 
-        Clothing[] items = {item0, item1, item2, item3};
-
-        System.out.println("\nBasket:");
+        System.out.println("\nShopping Basket:");
         for (Clothing item: items) {
             System.out.println(item.getDescription() + ", " + item.getPrice() + ", " + item.getSize());        
         }
-
-        System.out.println("\nNotice:");
-        System.out.println("Only the items of the costumer's size will be checked out.");
-        
-        //subtotal = amounts[0] * items[0].price + amounts[1] * items[1].price;
-        //taxes = subtotal * tax;
         
         for (int i = 0; i < items.length; i++) {
             if (items[i].getSize().equals(customer.getSize())) {            
@@ -54,5 +53,7 @@ public class ShopApp {
         }
         
         System.out.println("\nTotal: " + total);
+        System.out.println("Important: Only the items of the costumer's size have been checked out.");
+
     }
 }
